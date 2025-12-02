2b002bd6afc706f00ac93c049239a8dbb8eaea33
trungchien07 <trungchien332007@gmail.com>
2025-12-02 09:14:30 +0700

    ex43

---
+++ ex43/TestMain.java
@@ -0,0 +1,13 @@
+package ex43;
+
+public class TestMain {
+        public static void main(String[] args) {
+            Point2D p2 = new Point2D(3.5f, 4.2f);
+            Point3D p3 = new Point3D(1.2f, 3.4f, 5.6f);
+
+            System.out.println("Point2D: " + p2);
+            System.out.println("Point3D: " + p3);
+        }
+    }
+
+
