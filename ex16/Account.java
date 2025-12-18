840af2615c861df52311be8f10e2560133f330ed
trungchien07 <trungchien332007@gmail.com>
2025-12-18 08:57:41 +0700

    ex16

---
+++ ex16/Account.java
@@ -0,0 +1,54 @@
+package ex16;
+
+public class Account {
+    private String id;
+    private String name;
+    private int balance = 0;
+
+    public Account(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Account(String id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+    public int credit(int amount){
+        balance += amount;
+        return balance;
+    }
+    public int debit(int amount){
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Amount exceeded balance");
+        }
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    public void transferTo(Account a2, int i) {
+    }
+}
